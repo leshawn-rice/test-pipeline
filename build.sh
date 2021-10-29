bd34d2fb3e492731a66ed08ca07c241440049629
Leshawn Rice <leshawn.rice@outlook.com>
2021-10-29 15:56:34 -0700

    updated Jenkinsfile & build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 # This script will trigger a build. Put it in the root directory of the repo you want to trigger a build for. Pipelines should share names with repos
 
-jenkins_url=${JENKINS_URL:-'http://192.168.56.135'}
+jenkins_url=${JENKINS_URL:-'http://192.168.56.135:8080'}
 repository=$(basename $(pwd))
 token=${JENKINS_TOKEN:-'jenkins-token'}
 
+echo $token
 # Push the most recent commits to the current branch
 git push
-http://192.168.56.135:8080/job/Test%20pipeline/build?token=mytoken
-curl "$jenkins_url/job/$repository/build?token=$token"
+curl "$jenkins_url/buildByToken/build?job=$repository&token=$token"
 
 
